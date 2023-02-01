f641253497a44326e8ab34fe96b470a52f1c404e
R00tDev1l <74395487+Darkhaxxor005@users.noreply.github.com>
2023-02-01 19:33:24 +0600

    Update phphisher.py

--- phphisher.py
+++ phphisher.py
@@ -118,8 +118,8 @@ ______ _   _       _     _     _
 |  __/|  _  | '_ \| '_ \| / __| '_ \ / _ \ '__|
 | |   | | | | |_) | | | | \__ \ | | |  __/ |   
 \_|   \_| |_/ .__/|_| |_|_|___/_| |_|\___|_|   
-            | |                                
-            |_|   By {author} of PH squad."""
+            | |   Original Credit KasRoudra                          
+            |_|   Modified By {author} of PH squad."""
 
 nr_help = f"""
 {info}Steps: {nc}
